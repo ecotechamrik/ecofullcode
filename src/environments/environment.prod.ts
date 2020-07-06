4b9805fbcd6cad3a518462d0faa8548fe1bb303b
Amrik Singh <amriksg81@gmail.com>
2020-07-06 14:45:52 -0700

    Environment Production File Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  Cell:"604.677.1144",
+  OfficePhone:"1.888.238.2687",
+  Fax: "604.677.1146",
+  ContactEmail: "info@doorex.com",
+  WebsiteName: "Door Seattle",
+  DevelopedBy: "Department of MIS, Eco Tech Door",
+  CompanyName: "Eco Tech Doors"
+};
+
+/* Setting Different Websites Values depending on the Website Domain to load content of the selected website */
+export enum WEBSITES{
+  DoorSeattle = 1,
+  DoorsSeattle = 2,
+  EcoTechDoor = 3
 };
